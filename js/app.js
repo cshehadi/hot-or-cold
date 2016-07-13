a1735a3e591d30197e4be11ecdc4b2941621c87c
Charlie Shehadi <cshehadi@gmail.com>
2016-07-13 17:32:50 -0400

    first draft

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+'use strict';
 
-var count = 0;
+var max = 100;
+var guessCount = 0;
 var target;
 
 function setTarget() {
@@ -7,7 +9,7 @@ function setTarget() {
 }
 
 function setCounter() {
-    $('#count').html(count);
+    $('#count').html(guessCount);
 }
 
 function newGame() {
@@ -16,7 +18,56 @@ function newGame() {
     // clear out any previous guesses
     $('#guessList').html('');
     $('#userGuess').val('');
+    $('#feedback').html('Make your Guess!');
     setCounter();
+}
+
+function guessMessage(guess) {
+
+    if (guess == target) {
+        return "That's it!  Nice Job";
+    } else {
+        var distance = Math.abs(target - guess);
+
+        var messages = [
+            { diff: 0, msg: "that's it!  Nice Job!" }, 
+            { diff: 3, msg: 'blazing' }, 
+            { diff: 8, msg: 'hot' }, 
+            { diff: 16, msg: 'warm' }, 
+            { diff: 24, msg: 'lukewarm' }, 
+            { diff: 40, msg: 'a tad chilly' }, 
+            { diff: 60, msg: 'cold' }, 
+            { diff: 80, msg: 'frigid' } 
+            ];
+
+        for (var i = 0; i < messages.length; i++) {
+            if (distance <= messages[i].diff) {
+                return messages[i].msg;
+            }
+        }
+    }
+}
+
+function handleGuess(guess) {
+    // update the counter
+    guessCount++;
+    setCounter();
+
+    // update the list of guesses
+    $('#guessList').append('<li>'+guess+'</li>');
+
+    // let the user know how they did
+    $('#feedback').html(guessMessage(guess));
+}
+
+function validateGuess(guess) {
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+        return 'Please enter a number between 1 and 100';
+    }
+}
+
+function displayError(error) {
+    alert(error);
 }
 
 
@@ -36,6 +87,18 @@ $(document).ready(function(){
     /*--- Handle new game request ---*/
     $("a.new").click(function(){
         newGame();
+    });
+
+    /*--- Handle guess ---*/
+    $("#guessButton").click(function(){
+        var guess = parseInt($('#userGuess').val());
+        var error = validateGuess(guess);
+        if (!error) {
+            handleGuess(guess);
+        } else {
+            displayError(error);
+        }
+        return false;
     });
 
     newGame();
